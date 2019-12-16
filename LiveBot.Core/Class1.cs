c142f834c84ddb83365d9bd400a27171f773c91a
Brandon <git@bsquid.io>
2019-12-16 14:33:34 -0800

    Made some changes to what LiveBot.Core and LiveBot.Repository are

---
+++ LiveBot.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LiveBot.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ LiveBot.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LiveBot.Repository
+{
+    public class Class1
+    {
+    }
+}
