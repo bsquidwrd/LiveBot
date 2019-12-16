6a10b2b74231906d5c810955d9e6761c457e4491
Brandon <git@bsquid.io>
2019-12-16 14:16:16 -0800

    Initial commit of code

---
+++ LiveBot.API/Program.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace LiveBot.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            Log.Debug("This is debug");
+            Log.Information("This is information");
+            Log.Warning("This is warning");
+            Log.Error("This is error");
+
+            CreateHostBuilder(args).Build().Run();
+
+            Log.CloseAndFlush();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
