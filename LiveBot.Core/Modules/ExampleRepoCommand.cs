27a5b11dbe2d11859ce8e6c7e38c957db33dee1a
Justin Packwood <justinp917@gmail.com>
2019-12-19 09:08:23 -0500

    Example Repository Implementation

---
+++ LiveBot.Core/Modules/ExampleRepoCommand.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+using LiveBot.Core.Repository;
+
+namespace LiveBot.Core.Modules
+{
+    public class ExampleRepoCommand : ModuleBase<ShardedCommandContext>
+    {
+        private readonly IExampleRepository _exampleRepository;
+
+        public ExampleRepoCommand(IExampleRepository exampleRepository)
+        {
+            _exampleRepository = exampleRepository;
+        }
+
+        [Command("RepoExample")]
+        public async Task RepoExampleAsync()
+        {
+            _exampleRepository.RepoCall();
+            await ReplyAsync("Re called the repo method! Check the console!");
+        }
+    }
+}
