e6d5401de83f8fe4546462ee018338670b3e0ed7
Brandon <git@bsquid.io>
2020-06-10 13:24:18 -0700

    Update default to include role ping

--- LiveBot.Core/Repository/Static/Defaults.cs
+++ LiveBot.Core/Repository/Static/Defaults.cs
@@ -5,6 +5,6 @@ namespace LiveBot.Core.Repository.Static
     public static class Defaults
     {
         public static TimeSpan MessageTimeout = TimeSpan.FromMinutes(1);
-        public static string NotificationMessage = "{name} is live and is playing {game}! {url}";
+        public static string NotificationMessage = "{role} {name} is live and is playing {game}! {url}";
     }
 }
