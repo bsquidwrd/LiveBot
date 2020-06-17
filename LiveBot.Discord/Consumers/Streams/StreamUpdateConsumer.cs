24703f66ba3c4b4555fcbbf89bd0f91ebe9f5201
Brandon <git@bsquid.io>
2020-06-16 17:12:05 -0700

    Catch and print exception for better logging

--- LiveBot.Discord/Consumers/Streams/StreamUpdateConsumer.cs
+++ LiveBot.Discord/Consumers/Streams/StreamUpdateConsumer.cs
@@ -4,6 +4,8 @@ using LiveBot.Core.Repository.Interfaces;
 using LiveBot.Core.Repository.Interfaces.Monitor;
 using LiveBot.Core.Repository.Models.Streams;
 using MassTransit;
+using Serilog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -30,40 +32,46 @@ namespace LiveBot.Discord.Consumers.Streams
             // If so, send it out.
             // Because users are monitored on load, if someone gets a subscription setup after someone is live and they didn't exist before
             // it won't notify because they are being "updated"
-
-            ILiveBotStream stream = context.Message.Stream;
-            var streamUser = await _work.UserRepository.SingleOrDefaultAsync(i => i.ServiceType == stream.ServiceType && i.SourceID == stream.UserId);
-            var streamSubscriptions = await _work.SubscriptionRepository.FindAsync(i => i.User == streamUser);
-
-            if (streamSubscriptions.Count() == 0)
-                return;
-
-            List<StreamSubscription> unsentSubscriptions = new List<StreamSubscription>();
-
-            foreach (StreamSubscription streamSubscription in streamSubscriptions)
+            try
             {
-                var discordGuild = await _work.GuildRepository.SingleOrDefaultAsync(i => i == streamSubscription.DiscordGuild);
-                var discordChannel = await _work.ChannelRepository.SingleOrDefaultAsync(i => i == streamSubscription.DiscordChannel);
+                ILiveBotStream stream = context.Message.Stream;
+                var streamUser = await _work.UserRepository.SingleOrDefaultAsync(i => i.ServiceType == stream.ServiceType && i.SourceID == stream.UserId);
+                var streamSubscriptions = await _work.SubscriptionRepository.FindAsync(i => i.User == streamUser);
 
-                var previousNotifications = await _work.NotificationRepository.FindAsync(i =>
-                    i.ServiceType == stream.ServiceType &&
-                    i.User_SourceID == streamUser.SourceID &&
-                    i.DiscordGuild_DiscordId == discordGuild.DiscordId &&
-                    i.DiscordChannel_DiscordId == discordChannel.DiscordId &&
-                    i.Stream_SourceID == stream.Id &&
-                    i.Stream_StartTime == stream.StartTime &&
-                    i.Success == true
-                );
+                if (streamSubscriptions.Count() == 0)
+                    return;
 
-                if (previousNotifications.Count() > 0)
-                    continue;
+                List<StreamSubscription> unsentSubscriptions = new List<StreamSubscription>();
 
-                unsentSubscriptions.Add(streamSubscription);
+                foreach (StreamSubscription streamSubscription in streamSubscriptions)
+                {
+                    var discordGuild = await _work.GuildRepository.SingleOrDefaultAsync(i => i == streamSubscription.DiscordGuild);
+                    var discordChannel = await _work.ChannelRepository.SingleOrDefaultAsync(i => i == streamSubscription.DiscordChannel);
+
+                    var previousNotifications = await _work.NotificationRepository.FindAsync(i =>
+                        i.ServiceType == stream.ServiceType &&
+                        i.User_SourceID == streamUser.SourceID &&
+                        i.DiscordGuild_DiscordId == discordGuild.DiscordId &&
+                        i.DiscordChannel_DiscordId == discordChannel.DiscordId &&
+                        i.Stream_SourceID == stream.Id &&
+                        i.Stream_StartTime == stream.StartTime &&
+                        i.Success == true
+                    );
+
+                    if (previousNotifications.Count() > 0)
+                        continue;
+
+                    unsentSubscriptions.Add(streamSubscription);
+                }
+
+                if (unsentSubscriptions.Count() > 0)
+                {
+                    await _bus.Publish<IStreamOnline>(new { Stream = stream });
+                }
             }
-
-            if (unsentSubscriptions.Count() > 0)
+            catch (Exception e)
             {
-                await _bus.Publish<IStreamOnline>(new { Stream = stream });
+                Log.Error($"{e}");
             }
         }
     }
