88ca8136f3c885d3c0aacd126f0c4adbc0b0e99f
Brandon <git@bsquid.io>
2020-06-17 22:24:19 -0700

    Trying to get a web interface up and running

---
+++ LiveBot.API/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LiveBot.API.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
