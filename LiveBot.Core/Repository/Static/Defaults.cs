5bd2ac56a0c39463cba6786ffa7d961081eeeab9
Brandon <git@bsquid.io>
2022-03-13 16:37:03 -0700

    Change type of default notification message top use as parameter default

--- LiveBot.Core/Repository/Static/Defaults.cs
+++ LiveBot.Core/Repository/Static/Defaults.cs
@@ -5,6 +5,6 @@ namespace LiveBot.Core.Repository.Static
     public static class Defaults
     {
         public static readonly TimeSpan MessageTimeout = TimeSpan.FromMinutes(1);
-        public static readonly string NotificationMessage = "{role} {name} is live and playing {game}! {url}";
+        public const string NotificationMessage = "{role} {name} is live and playing {game}! {url}";
     }
 }
