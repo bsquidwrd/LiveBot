78b9172470dea415b99657897666dbacc3bdd56d
Brandon <git@bsquid.io>
2022-03-24 21:32:39 -0700

    Add perm check command so people can ensure the bot has the right permissions for a channel

--- LiveBot.Discord.SlashCommands/Modules/GeneralModule.cs
+++ LiveBot.Discord.SlashCommands/Modules/GeneralModule.cs
@@ -1,4 +1,5 @@
-﻿using Discord.Interactions;
+﻿using Discord;
+using Discord.Interactions;
 using Discord.Rest;
 
 namespace LiveBot.Discord.SlashCommands.Modules
@@ -7,6 +8,36 @@ namespace LiveBot.Discord.SlashCommands.Modules
     {
         [SlashCommand(name: "ping", description: "Ping the bot")]
         public async Task PingAsync() =>
-            await RespondAsync("Pong!", ephemeral: true);
+            await FollowupAsync(text: "Pong!", ephemeral: true);
+
+        [SlashCommand(name: "perm-check", description: "Confirm the bot has the necessary permissions to post in a channel")]
+        public async Task PermCheckAsync(ITextChannel? channel = null)
+        {
+            if (channel == null)
+                channel = (ITextChannel)Context.Channel;
+
+            var guildUser = await Context.Guild.GetCurrentUserAsync();
+            var perms = guildUser.GetPermissions(channel);
+
+            var missingPermissions = new List<string>();
+
+            if (!perms.ViewChannel)
+                missingPermissions.Add("View Channel");
+            if (!perms.SendMessages)
+                missingPermissions.Add("Send Messages");
+            if (!perms.EmbedLinks)
+                missingPermissions.Add("Embed Links");
+            if (!perms.UseExternalEmojis)
+                missingPermissions.Add("Use External Emojis");
+            if (!perms.ReadMessageHistory)
+                missingPermissions.Add("Read Message History");
+            if (!perms.MentionEveryone)
+                missingPermissions.Add("Mention Everyone");
+
+            var permissionsResult = "All permissions are set correctly!";
+            if (missingPermissions.Any())
+                permissionsResult = $"Missing Permissions in {channel.Mention}: {string.Join(", ", missingPermissions)}";
+            await FollowupAsync(text: permissionsResult, ephemeral: true);
+        }
     }
 }
