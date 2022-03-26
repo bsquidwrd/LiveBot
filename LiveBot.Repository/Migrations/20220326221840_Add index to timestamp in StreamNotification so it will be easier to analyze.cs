0e789302a416c9f1587e6c7d1096ffc87d349d1e
Brandon <git@bsquid.io>
2022-03-26 15:23:47 -0700

    Add index to TimeStamp column in StreamNotification

---
+++ LiveBot.Repository/Migrations/20220326221840_Add index to timestamp in StreamNotification so it will be easier to analyze.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LiveBot.Repository.Migrations
+{
+    public partial class AddindextotimestampinStreamNotificationsoitwillbeeasiertoanalyze : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_StreamNotification_Timestamp",
+                table: "StreamNotification",
+                column: "TimeStamp"
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_StreamNotification_Timestamp",
+                table: "StreamNotification"
+            );
+        }
+    }
+}
