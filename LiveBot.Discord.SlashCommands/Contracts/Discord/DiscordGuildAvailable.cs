dd499438682892559b8b0fcf715cc7aa28eff8ac
Brandon <git@bsquid.io>
2022-03-29 18:09:46 -0700

    Add DiscordGuildAvailable for being able to fire events from slash commands

---
+++ LiveBot.Discord.SlashCommands/Contracts/Discord/DiscordGuildAvailable.cs
@@ -0,0 +1,11 @@
+﻿using LiveBot.Core.Contracts.Discord;
+
+namespace LiveBot.Discord.SlashCommands.Contracts.Discord
+{
+    public class DiscordGuildAvailable : IDiscordGuildAvailable
+    {
+        public ulong GuildId { get; set; }
+        public string GuildName { get; set; }
+        public string IconUrl { get; set; }
+    }
+}
