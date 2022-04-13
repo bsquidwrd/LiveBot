55fa9e6a3990e1b4a88b5260eb59db23e37651e6
Brandon <git@bsquid.io>
2022-04-12 18:28:59 -0700

    Add YouTube and Trovo coloring to support Role monitoring

--- LiveBot.Core/Repository/Static/ServiceEnum.cs
+++ LiveBot.Core/Repository/Static/ServiceEnum.cs
@@ -1,4 +1,7 @@
-﻿namespace LiveBot.Core.Repository.Static
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace LiveBot.Core.Repository.Static
 {
     /// <summary>
     /// Represents a Monitoring Service
@@ -7,6 +10,8 @@
     {
         None = 0,
         Twitch = 1,
+        YouTube = 2,
+        Trovo = 3,
     }
 
     public static class ServiceUtils
@@ -15,7 +20,25 @@
             serviceEnum switch
             {
                 ServiceEnum.Twitch => 0x9146FF,
+                ServiceEnum.YouTube => 0xF80000,
+                ServiceEnum.Trovo => 0x37BB76,
                 _ => 0xFFFFFF,
             };
+
+        public const string TwitchUrlPattern = "^((http|https):\\/\\/|)([\\w\\d]+\\.)?twitch\\.tv/(?<username>[a-zA-Z0-9_]{1,})";
+        public const string YouTubeUrlPattern = "^((?:https?:)?\\/\\/)?((?:www|m)\\.)?((?:youtube(-nocookie)?\\.com|youtu.be))(\\/(?:[\\w\\-]+\\?v=|embed\\/|v\\/)?)([\\w\\-]+)(\\S+)?$";
+        public const string TrovoUrlPattern = "^((http|https):\\/\\/|)([\\w\\d]+\\.)?trovo\\.live/(?<username>[a-zA-Z0-9_]{1,})";
+
+        public static ServiceEnum ToServiceEnum(this Uri uri)
+        {
+            if (Regex.IsMatch(uri.AbsoluteUri, TwitchUrlPattern))
+                return ServiceEnum.Twitch;
+            else if (Regex.IsMatch(uri.AbsoluteUri, YouTubeUrlPattern))
+                return ServiceEnum.YouTube;
+            else if (Regex.IsMatch(uri.AbsoluteUri, TrovoUrlPattern))
+                return ServiceEnum.Trovo;
+            else
+                return ServiceEnum.None;
+        }
     }
 }
